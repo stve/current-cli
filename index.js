b0afdda7422196edb0349673a5fbf3164f8a5f35
Steve Agalloco <steve.agalloco@gmail.com>
2017-09-07 11:10:55 -0400

    rework cli to use commander

--- index.js
+++ index.js
@@ -4,20 +4,27 @@
 const log = console.log;
 const path = require('path');
 
+const program = require('commander');
+
 const { configuration } = require('./lib/config');
 const { LANGUAGES } = require('./lib/languages');
 const { current, bolt } = require('./lib/utils');
 
-if (configuration.isConfigured()) {
-  let settings = configuration.loadSettings();
+let langValue;
 
-  let langs = LANGUAGES.filter((l) => {
-    return settings.languages.hasOwnProperty(l.name) && settings.languages[l.name]
+program
+  .version('0.1.0')
+  .option('-c, --configure', 'Configure');
+
+program
+  .arguments('<language>')
+  .action((l) => {
+     langValue = l;
   });
 
-  current(langs);
+program.parse(process.argv);
 
-} else {
+function configure() {
   bolt("Tell current which languages you'd like to report on...\n");
 
   const Enquirer = require('enquirer');
@@ -37,6 +44,79 @@ if (configuration.isConfigured()) {
 
       configuration.saveSettings(settings);
 
-      bolt("\nSettings saved!")
+      bolt("Settings saved!")
     });
+}
+
+function list() {
+  let settings = configuration.loadSettings();
+
+  let langs = LANGUAGES.filter((l) => {
+    return settings.languages.hasOwnProperty(l.name) && settings.languages[l.name]
+  });
+
+  current(langs);
+}
+
+function lookup(lang) {
+  lang = lang.toLowerCase();
+  let langs = LANGUAGES.filter((l) => {
+    return l.name === lang;
+  });
+
+  if (langs.length > 0) {
+    current(langs);
+  } else {
+    bolt(`${lang} not found`);
+  }
+}
+
+if (program.configure) {
+  configure();
+  return;
+}
+
+if (typeof langValue === "undefined") {
+  if (configuration.isConfigured()) {
+    list();
+  } else {
+    configure();
+  }
+} else {
+  lookup(langValue);
+}
+
+if (program.list) {
+  if (configuration.isConfigured()) {
+    let settings = configuration.loadSettings();
+
+    let langs = LANGUAGES.filter((l) => {
+      return settings.languages.hasOwnProperty(l.name) && settings.languages[l.name]
+    });
+
+    current(langs);
+
+  } else {
+    bolt("Tell current which languages you'd like to report on...\n");
+
+    const Enquirer = require('enquirer');
+    const enquirer = new Enquirer();
+    enquirer.register('confirm', require('prompt-confirm'));
+
+    let settings = {};
+
+    LANGUAGES.forEach((language) => {
+      enquirer.question(language.name, `${language.name}?`, {type: 'confirm'});
+    });
+
+    enquirer
+      .prompt(LANGUAGES.map(l => l.name))
+      .then(function(answers) {
+        settings.languages = answers;
+
+        configuration.saveSettings(settings);
+
+        bolt("\nSettings saved!")
+      });
+  }
 }
