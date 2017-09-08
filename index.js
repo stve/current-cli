25a1f839467b8766cfc69786288af2944e4f8237
Steve Agalloco <steve.agalloco@gmail.com>
2017-09-08 17:35:20 -0400

    split out commands into separate files

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const Enquirer = require('enquirer');
-const prompt = require('prompt-confirm');
 
 const { configuration } = require('./lib/config');
-const { LANGUAGES } = require('./lib/languages');
-const { current, bolt } = require('./lib/utils');
+const { configure, list, lookup } = require('./lib/commands');
 
 let langValue;
 
@@ -21,48 +18,6 @@ program
   });
 
 program.parse(process.argv);
-
-function configure() {
-  bolt("Tell current which languages you'd like to report on...\n");
-
-  const enquirer = new Enquirer();
-  enquirer.register('confirm', prompt);
-
-  const settings = {};
-
-  LANGUAGES.forEach((language) => {
-    enquirer.question(language.name, `${language.name}?`, { type: 'confirm' });
-  });
-
-  enquirer
-    .prompt(LANGUAGES.map(l => l.name))
-    .then((answers) => {
-      settings.languages = answers;
-
-      configuration.saveSettings(settings);
-
-      bolt('Settings saved!');
-    });
-}
-
-function list() {
-  const settings = configuration.loadSettings();
-
-  const langs = LANGUAGES.filter(l => settings.languages[l.name]);
-
-  current(langs);
-}
-
-function lookup(lang) {
-  const lookupValue = lang.toLowerCase();
-  const langs = LANGUAGES.filter(l => l.name === lookupValue);
-
-  if (langs.length > 0) {
-    current(langs);
-  } else {
-    bolt(`${lang} not found`);
-  }
-}
 
 if (typeof langValue !== 'undefined') {
   lookup(langValue);
